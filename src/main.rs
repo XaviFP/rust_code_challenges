b034f7a1109a80106cf1c3ac82d4f6ac8f9d98f3
Xavi Ferrer <xavi@delape.net>
2022-11-29 19:52:42 +0100

    split logic into function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::env;
 
-
 fn main() {
-    //let numbers: Vec<f32> = Vec::new();
     let result: Option<f32> = None;
     
     let args = env::args();
@@ -11,12 +9,29 @@ fn main() {
         return
     }
 
-    let mut numbers = args.skip(1).fold(Vec::new(),|mut numbers, arg| {
+    let numbers = args.skip(1).fold(Vec::new(),|mut numbers, arg| {
         let number = arg.parse::<f32>().expect("args should be f32");
         numbers.push(number);
         numbers
     });
 
-    numbers.sort();
+    dbg!(median(numbers).unwrap());
+}
 
+fn median(mut numbers: Vec<f32>) -> Option<f32> {
+    let len = numbers.len();
+    if len == 0 {
+        return None
+    }
+
+    numbers.sort_by(|a, b|{a.partial_cmp(b).unwrap()});
+    let med: f32;
+    let middle = len /2;
+    if len % 2 != 0 {
+        med = numbers[middle];
+    } else {
+        med = (numbers[middle - 1] + numbers[middle]) / 2.0;
+    }
+
+    Some(med)
 }
